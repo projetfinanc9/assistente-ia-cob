1d69294dee2c317858319180ffd8411492b7e7fd
igorkaik320 <igorkaik320@gmail.com>
2026-07-09 11:08:29 -0300

    Remover componentes de chat e Supabase não utilizados

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Auth from "./pages/Auth";
 import Settings from "./pages/Settings";
 import CobrancaConfig from "./pages/CobrancaConfig";
 import Dashboard from "./pages/Dashboard";
@@ -22,7 +21,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/auth" element={<Auth />} />
           <Route path="/dashboard" element={<DashboardLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="history" element={<History />} />
