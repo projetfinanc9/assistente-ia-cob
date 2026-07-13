a6c3e1c796cae3f02b3f421bb044fbbe818d789d
projetfinanc9 <projetfinanc9@gmail.com>
2026-07-13 15:56:55 -0300

    Trocar BrowserRouter por HashRouter para corrigir rotas no Vercel
    - HashRouter usa hash na URL (#/history) em vez de path (/history)
    - Isso evita problemas com roteamento no servidor do Vercel
    - Todas as rotas devem funcionar agora

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Settings from "./pages/Settings";
 import CobrancaConfig from "./pages/CobrancaConfig";
 import Dashboard from "./pages/Dashboard";
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<DashboardLayout />}>
             <Route index element={<Dashboard />} />
@@ -28,7 +28,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
