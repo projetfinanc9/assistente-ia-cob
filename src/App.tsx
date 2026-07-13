0f38c9178bc5d9505a6a49d712307e21e9aee71c
projetfinanc9 <projetfinanc9@gmail.com>
2026-07-13 15:44:36 -0300

    Remover tela inicial e ir direto ao dashboard
    - Alterar rota principal para carregar DashboardLayout com Dashboard
    - Remover import não utilizado do Index

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import Settings from "./pages/Settings";
 import CobrancaConfig from "./pages/CobrancaConfig";
 import Dashboard from "./pages/Dashboard";
@@ -20,8 +19,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="/" element={<DashboardLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="history" element={<History />} />
             <Route path="cobranca-config" element={<CobrancaConfig />} />
